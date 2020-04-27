be0f1326bbad56b5569650971e9c7b4f8f2d082b
Jacob Morris <Jacob.Morris@agreliant.com>
2020-04-26 20:06:28 -0400

    Cleaned up code, added list of available stock markets

--- stocks/stocks_mvc/views.py
+++ stocks/stocks_mvc/views.py
@@ -1,6 +1,17 @@
+import requests
+import json
+from stocks import settings
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        token = settings.FINNHUB_API_KEY
+        r = requests.get(
+                'https://finnhub.io/api/v1/stock/exchange?token=' + token)
+
+        print(token)
+        json = r.json()
+
+        return render(request, 'index.html', { 'data': json })
